3a01020b2e787283b4b539ad8267df89f98e4f50
Samman <asamman560@gmail.com>
2025-11-25 19:11:09 +0200

    Sorting Data

---
+++ 4 Sorting/4 Sorting/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace _4_Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Country", typeof(string));
+            dt.Columns.Add("Salary", typeof(Double));
+            dt.Columns.Add("Date", typeof(DateTime));
+
+            dt.Rows.Add(1, "Mohamed Abu-Hadhoud", "Jordon", 5000, DateTime.Now);
+            dt.Rows.Add(2, "Ahmed Samman", "Egypt", 525.5, DateTime.Now);
+            dt.Rows.Add(3, "Fadi Ali", "USA", 730.5, DateTime.Now);
+            dt.Rows.Add(4, "Gehad Farok", "London", 800, DateTime.Now);
+            dt.Rows.Add(5, "Kamel Tamer", "Jordon", 7000, DateTime.Now);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Console.WriteLine("ID: {0} \t Name: {1} \t Country: {2} \t Salary: {3} \t Date: {4} \t"
+                    , row["ID"], row["Name"], row["Country"], row["Salary"], row["Date"]);
+            }
+
+            //------------------------------------------//
+
+            dt.DefaultView.Sort = "ID DESC";
+            dt = dt.DefaultView.ToTable();
+
+            Console.WriteLine("\nSorting By Id DESC");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Console.WriteLine("ID: {0} \t Name: {1} \t Country: {2} \t Salary: {3} \t Date: {4} \t"
+                    , row["ID"], row["Name"], row["Country"], row["Salary"], row["Date"]);
+            }
+
+            //-----------------------------------------//
+
+            dt.DefaultView.Sort = "Name ASC";
+            dt = dt.DefaultView.ToTable();
+
+            Console.WriteLine("\nSorting By Name ASC");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Console.WriteLine("ID: {0} \t Name: {1} \t Country: {2} \t Salary: {3} \t Date: {4} \t"
+                    , row["ID"], row["Name"], row["Country"], row["Salary"], row["Date"]);
+            }
+
+
+        }
+    }
+}
